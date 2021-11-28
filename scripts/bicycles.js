9017382323c2e902f8d962460e95a1844592f278
Artyom Khaov <a.khaov@gmail.com>
2021-11-28 17:40:24 +0700

    Added: intro styles structure, surfaces template

--- scripts/bicycles.js
+++ scripts/bicycles.js
@@ -1,65 +1,41 @@
-const bicyclesItems = [
-  {
-    surface: 'Шоссе',
-    active: 'true',
-    data: [
-      {
-        link: 'https://www.sigmasports.com/item/Cervelo/Caledonia-5-Ultegra-Disc-Road-Bike-2021/RDEN',
-        image: './images/bicycles/bicycle-cervelo-caledonia-5.png',
-        model: 'Cervelo Caledonia 5'
-      },
-      {
-        link: 'https://www.sigmasports.com/item/Cannondale/SystemSix-HiMOD-Ultegra-Di2-Disc-Road-Bike-2021/R82J',
-        image: './images/bicycles/bicycle-cannondale-systemsix-himod.png',
-        model: 'Cannondale Systemsix Himod'
-      },
-      {
-        link: 'https://www.sigmasports.com/item/Trek/Domane-SL-7-Force-eTap-AXS-Disc-Road-Bike-2021/RULF',
-        image: './images/bicycles/bicycle-trek-domane-sl-7.png',
-        model: 'Trek Domane SL-7'
-      }
-    ]
-  },
-  {
-    surface: 'Грэвел',
-    active: 'false',
-    data: [
-      {
-        link: 'https://www.sigmasports.com/item/Cervelo/Aspero-GRX-810-1x-Disc-Gravel-Bike-2021/RJDE',
-        image: './images/bicycles/bicycle-cervelo-aspero-grx-810.png',
-        model: 'Cervelo Aspero GRX 810'
-      },
-      {
-        link: 'https://www.sigmasports.com/item/Specialized/S-Works-Diverge-Gravel-Bike-2020/NVJ9',
-        image: './images/bicycles/bicycle-specialized-s-works-diverge.png',
-        model: 'Specialized S-Works Diverge'
-      },
-      {
-        link: 'https://www.sigmasports.com/item/Cannondale/Topstone-Carbon-Lefty-3-Disc-Gravel-Road-Bike-2021/PUC8',
-        image: './images/bicycles/bicycle-cannondale-topstone-lefty-3.png',
-        model: 'Cannondale Topstone Lefty 3'
-      }
-    ]
-  },
-  {
-    surface: 'TT',
-    active: 'false',
-    data: [
-      {
-        link: 'https://www.sigmasports.com/item/Specialized/S-Works-Shiv-Disc-Limited-Edition-Triathlon-Bike-2019/K8P9',
-        image: './images/bicycles/bicycle-specialized-s-works-shiv.png',
-        model: 'Specialized S-Works Shiv'
-      },
-      {
-        image: './images/bicycles/bicycle-bmc-timemachine-01-one.png',
-        model: 'BMC Timemachine 01 ONE',
-        link: 'https://www.sigmasports.com/item/BMC/Timemachine-01-One-Force-Disc-TT-Triathlon-Bike-2021/S835'
-      },
-      {
-        image: './images/bicycles/bicycle-cervelo-p-series.png',
-        model: 'Cervelo P-Series',
-        link: 'https://www.sigmasports.com/item/Cervelo/P-Series-Ultegra-Di2-TT-Triathlon-Bike-2021/RM6Q'
-      }
-    ]
-  }
-];
+const bicyclesList = document.querySelector('.bicycles__list');
+const bicycleTemplate = document.querySelector('.bicycle-template').content;
+
+// Create bicycles item
+
+function createBicyclesItem(imageValue, modelValue, linkValue) {
+  const bicyclesItem = bicycleTemplate.querySelector('.bicycles__item').cloneNode(true);
+  const bicycleLink = bicyclesItem.querySelector('.bicycle__link');
+  const bicycleImage = bicyclesItem.querySelector('.bicycle__image');
+  const bicycleModel = bicyclesItem.querySelector('.bicycle__model');
+
+  bicycleLink.href = linkValue;
+  bicycleLink.title = modelValue;
+  bicycleImage.src = imageValue;
+  bicycleImage.alt = modelValue;
+  bicycleModel.textContent = modelValue;
+
+  return bicyclesItem;
+}
+
+// Render bicycles item
+
+function renderBicyclesItem(bicycleLink, bicycleImage, bicycleModel) {
+  bicyclesList.append(createBicyclesItem(bicycleImage, bicycleModel, bicycleLink));
+}
+
+// Load bicycles items
+
+function loadBicyclesItems(bicyclesData) {
+  const activeBicycles = bicyclesData.filter(function (category) {
+    if (category.active === 'true') {
+      return category.data;
+    }
+  });
+
+  activeBicycles[0]['data'].forEach(function (bicycle) {
+    renderBicyclesItem(bicycle.link, bicycle.image, bicycle.model);
+  })
+}
+
+loadBicyclesItems(bicyclesItems);
