f26567b5398923eb9faff953bb50610f9cc2d178
Artyom Khaov <a.khaov@gmail.com>
2021-11-30 23:36:07 +0700

    Fixed: surfaces slider

--- scripts/surfaces.js
+++ scripts/surfaces.js
@@ -1,17 +1,15 @@
 const surfacesList = document.querySelector('.surfaces__list');
+const surfaceTitle = document.querySelector('.surface__title');
+const surfaceDescription = document.querySelector('.surface__description');
 const surfaceTemplate = document.querySelector('.surface-template').content;
 
 // Create surfaces item
 
 function createSurfacesItem(titleValue, descriptionValue, imageValue, iconValue) {
   const surfacesItem = surfaceTemplate.firstElementChild.cloneNode(true);
-  const surfaceTitle = surfacesItem.querySelector('.surface__title');
-  const surfaceDescription = surfacesItem.querySelector('.surface__description');
   const surfaceImage = surfacesItem.querySelector('.surface__image');
   const surfaceIcon = surfacesItem.querySelector('.surface__icon');
 
-  surfaceTitle.textContent = titleValue;
-  surfaceDescription.textContent = descriptionValue;
   surfaceImage.src = imageValue;
   surfaceImage.alt = titleValue;
   surfaceIcon.src = iconValue;
@@ -34,12 +32,19 @@ surfacesItems.forEach(function (surface) {
 
 // Initialize surfaces slider
 
-const surfacesSlider = new Swiper(".surfaces__container", {
+const surfacesSlider = new Swiper(".surfaces__slider", {
   slidesPerView: 2,
   spaceBetween: 40,
   loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
+  },
+  on: {
+    slideChange: function () {
+      const surfaceIndex = this.realIndex;
+      surfaceTitle.textContent = surfacesItems[surfaceIndex].title;
+      surfaceDescription.textContent = surfacesItems[surfaceIndex].description;
+    },
   }
 });
